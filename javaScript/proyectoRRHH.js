22059ad6c299522a4066e0f012f49c354e9eb8cf
Lautaro <laucarrizo332@gmail.com>
2022-10-18 18:30:09 -0300

    Primer Commit

---
+++ javaScript/proyectoRRHH.js
@@ -0,0 +1,53 @@
+// //IDS NAVEGACION DEL CITIO//
+// let boton1 = document.getElementById ("boton1")
+// let boton2 = document.getElementById ("boton2")
+// let boton3 = document.getElementById ("boton3")
+// let boton4 = document.getElementById ("boton4")
+// let boton5 = document.getElementById ("boton5")
+// let boton6 = document.getElementById ("boton6")
+// let boton7 = document.getElementById ("boton7")
+// let boton8 = document.getElementById ("boton8")
+// let boton9 = document.getElementById ("boton9")
+// let boton10 = document.getElementById ("boton10")
+// let boton11 = document.getElementById ("boton11")
+// let boton12= document.getElementById ("boton12")
+// //INDEX// INICIO//
+// boton1.onclick = ()=> {
+//     location.href ="./pages/nosotros.html"
+// }
+// boton2.onclick = ()=>{
+//     location.href ="./pages/servicio.html"
+// }
+// boton3.onclick = ()=>{
+//     location.href ="./pages/contacto.html"
+// }
+// //NOSOTROS//
+// boton4.addEventListener("click", function(){
+//     location.href ="../index.html"
+// })
+// boton5.addEventListener("click", function(){
+//     location.href ="../pages/servicio.html"
+// })
+// boton6.addEventListener("click", function(){
+//     location.href ="../pages/contacto.html"
+// })
+// //SERVICIOS//
+// boton7.addEventListener("click", function(){
+//     location.href ="../index.html"
+// })
+// boton8.addEventListener("click", function(){
+//     location.href ="../pages/nosotros.html"
+// })
+// boton9.addEventListener("click", function(){
+//     location.href ="../pages/contacto.html"
+// })
+// //CONTACTO//
+// boton10.addEventListener("click", function(){
+//     location.href ="../index.html"
+// })
+// boton11.addEventListener("click", function(){
+//     location.href ="../pages/nosotros.html"
+// })
+// boton12.addEventListener("click", function(){
+//     location.href ="../pages/servicios.html"
+// })
